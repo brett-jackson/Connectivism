88ba1c36c5fa760a4a399ad4bc1bce0f1a438414
Brett Jackson <brettjackson18@tamu.edu>
2020-01-27 14:07:49 -0600

    Started multithreading and GenNet design

--- GenNet.py
+++ GenNet.py
@@ -2,9 +2,12 @@
 """
 Created on Wed Jan 22 18:25:17 2020
 Self generating neural networks using Node class. 
+Individual node must wait for all prior nodes to compute.
 @author: Brett
 """
 from Node import DirectedNode
+from threading import Thread
+import asyncio
 
 class GenNode(DirectedNode):
     def __init__(self,actFunction,nid=0,value=0):
@@ -16,8 +19,12 @@ class GenNode(DirectedNode):
         self.backMean = 0
         self.backCount = 0
         self.act = actFunction
-    
+        self.sema = asyncio.Semaphore()
     def forward():
+        await self.sema.acquire()
+        try:
+            
+        finally:
         inVal = 0
         for node in self.inbound:
             
